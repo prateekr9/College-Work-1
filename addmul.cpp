0d5190c07bbed09ed7ab360dfaba2993c696b4c4
prateekr9 <48003668+prateekr9@users.noreply.github.com>
2019-02-26 10:28:47 +0530

    addmul.cpp

---
+++ addmul.cpp
@@ -0,0 +1,15 @@
+#include<iostream.h>
+#include<conio.h>
+
+void main()
+{
+int n1,n2;
+cout<<"Enter Number 1: "; cin>>n1;
+cout<<"Enter Number 2: "; cin>>n2;
+
+int addresult=n1+n2;
+int mulresult=n1*n2;
+cout<<"Addition Result= "<<addresult;
+cout<<"Multiplication Result= "<<mulresult;
+getch();
+}
