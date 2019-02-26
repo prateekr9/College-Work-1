d51d37f8cabdd290a832bc33e9e4475a40060182
prateekr9 <48003668+prateekr9@users.noreply.github.com>
2019-02-26 10:25:19 +0530

    add.cpp

---
+++ add.cpp
@@ -0,0 +1,13 @@
+#include<iostream.h>
+#include<conio.h>
+
+void main()
+{
+int n1,n2;
+cout<<"Enter Number 1: "; cin>>n1;
+cout<<"Enter Number 2: "; cin>>n2;
+
+int result=n1+n2;
+cout<<"Result= "<<result;
+getch();
+}
